68c7a6d420b4110c66062ffa7f6ff4c654644397
Irfan Akbari Habibi <irfanakbarihabibi18@gmail.com>
2023-06-15 14:13:53 +0700

    loading status

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -2,7 +2,7 @@ import {AiFillGoogleCircle} from "react-icons/ai";
 import {useRouter} from "next/router";
 import {toast} from "react-toastify";
 import Head from "next/head";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {SessionContext} from "@/context/SessionProvider";
 import login from "@/utils/firebase";
 import Layout from "@/components/Layout";
@@ -12,11 +12,13 @@ import axios from "axios";
 
 export default function Homepage() {
     const user = useContext(SessionContext);
+    const [isLoading, setIsLoading] = useState(false)
 
     const router = useRouter()
 
     const startbatch = async (e) => {
         e.preventDefault();
+        setIsLoading(true)
         try {
             await axios.get('https://asia-southeast2-traffix-cloud.cloudfunctions.net/batch-start-monitoring/start')
             toast.success("Sukses Menjalankan Monitoring", {
@@ -41,6 +43,7 @@ export default function Homepage() {
                 theme: "colored",
             });
         } finally {
+            setIsLoading(false)
         }
     }
     return (
@@ -54,9 +57,11 @@ export default function Homepage() {
                         className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <div className={"flex-col flex items-center justify-center"}>
-                                <button
+                                <button disabled={isLoading}
                                     className="flex items-center justify-center py-2 px-4 bg-red-600 text-white rounded-lg shadow-md" onClick={startbatch}>
-                                    Run Batch Monitoring
+                                    {
+                                        !isLoading ? 'Run Batch Monitoring' : 'Loading... Please wait'
+                                    }
                                 </button>
                                 <br/>
                                 <span>Monitoring berjalan secara otomatis setiap 30 menit</span>
